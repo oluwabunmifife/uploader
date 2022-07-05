69e53d81449ac259e900bdd5ab621dd935eb43bc
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-07-05 14:18:27 +0100

    Created model form for image file upload

--- imgUploader/models.py
+++ imgUploader/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from email.mime import image
 from django.db import models
+from django.contrib.auth.models import User
+
+# def user_directory_path(instance, filename):
+#     return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 
 # Create your models here.
+class Images(models.Model):
+    file = models.ImageField(#upload_to=user_directory_path,
+    width_field=100, height_field=100, blank=True, null=True)
+    name = models.CharField(max_length=30)
+    description = models.TextField(help_text="Give a short description of the image", max_length=100)
+
+
+    def __str__(self):
+        return self.name
---
+++ imgUploader/urls.py
@@ -0,0 +1,9 @@
+from django.urls import URLPattern
+from django.urls import path
+from . import views
+
+app_name = 'imgUploader'
+
+urlpatterns = [
+    path('', views.imgUpload, name='Upload Images'),
+]
